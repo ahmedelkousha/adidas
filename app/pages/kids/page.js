4b793696e00e2d1e04fc634178731a603b36ec15
ahmedelkousha <elkoushaservice@gmail.com>
2024-12-01 04:07:03 +0200

    files added

---
+++ app/pages/kids/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Kids() {
+  return (
+    <div className='bg-black text-white'>
+        <h2>
+            KIDS PAGE
+        </h2>
+    </div>
+  )
+}
